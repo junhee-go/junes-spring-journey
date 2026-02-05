af1b8e9ea982a55ede6001f92a2644083ea1d66f
junhee-go <junhee.go.io@gmail.com>
2026-02-05 21:16:41 +0900

    ch4-ex1: comment feature

--- ch4-ex1/src/main/java/main/Main.java
+++ ch4-ex1/src/main/java/main/Main.java
@@ -13,13 +13,17 @@ public class Main {
         // 기능 변경하고 싶다면?
         // interface 추가로 구현해서 클래스 만들고, 해당 클래스의 인스턴스 생성한 다음,
         // 그 인스턴스를 CommentService의 생성자 매개변수로 전달해주면 끝
+
+        // 의존성을 제공하여 Service Class Instance를 생성함
         var commentService = new CommentService(commentRepository,
                 commentNotificationProxy);
 
+        // 댓글 게시 사용 사례를 위해 매개변수로 전달할 댓글 인스턴스 생성
         var comment = new Comment();
         comment.setText("Demo Comment");
         comment.setAuthor("Lucy");
 
+        // 댓글 게시 사용 사례를 호출!
         commentService.publishComment(comment);
     }
 }
